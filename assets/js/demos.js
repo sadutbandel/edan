49024eb00b178f7879406c2b84aefbc9f6cff7af
wlemahieu <wesleylemahieu@gmail.com>
2016-03-24 20:50:39 -0400

    Fixing Demo pt.2

--- assets/js/demos.js
+++ assets/js/demos.js
@@ -25,7 +25,7 @@
   			default: {
   				disabled: false,
   				icon: 'add circle',
-  				title: 'Pay',
+  				title: 'Simulate Payment',
   				class: 'blue'
   			},
 
@@ -34,39 +34,28 @@
   				icon: 'loading spinner',
   				title: 'Paying',
   				class: 'grey'
-  			},
-
-  			paid: {
-  				disabled: true,
-  				icon: 'thumbs up',
-  				title: 'Paid',
-  				class: 'green'
   			}
   		};
 
   		$scope.button = button.default;
 
-  		$scope.submit = function () {
+  		$scope.simulatePayment = function () {
 
 			$scope.button = button.paying;
 
 			$http.post('/demo').success(function(data) {
 
-				$timeout(function() {
-
-					if(data.statusCode === 200) {
-						$scope.button = button.paid;
-						$scope.paid = true;
-					} else {
-						$scope.button = button[data.message];
-					}
-
+				if(data.statusCode === 200) {
+					$scope.paid = true;
+					$scope.account = null;
+					$scope.button = button.default;
+				} else {
+					$scope.button = button[data.message];
 					$timeout(function() {
 						$scope.account = null;
 						$scope.button = button.default;
 					}, 3000);
-
-				}, 3000);
+				}
 			})
 			.error(function(data, status) {
 				console.error('Error', status, data);
@@ -120,15 +109,10 @@
 
 			$scope.initialized = true; // triggers showing of demo itself. only reset on refresh or demo "quit"
 			$scope.payment_account = undefined; // no payment account by default
-			$scope.showSimulatePayment = false; // the form for simulating a payment using the free-rai form
 			$scope.paid = false; // not paid by default
 			$scope.paymentBegin().then(function() {
 				$scope.paymentWait();
 			});
-		}
-
-		$scope.simulatePayment = function() {
-			$scope.showSimulatePayment = true;
 		}
 	});
 
