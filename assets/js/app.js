c8fe082c04543d6dbc4e0a1ad381db07ba2f0b44
wlemahieu <wesleylemahieu@gmail.com>
2016-03-30 22:28:00 -0400

    Update wallet v7.4.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,7 +40,7 @@
 		// establish public app-wide variables
 		$rootScope.recaptchaKey = '6LcPNAsTAAAAANCpxZY3SMikIjg5a0T9XTnjM-v4';
 		$rootScope.faucetNumber = 'xrb_35jjmmmh81kydepzeuf9oec8hzkay7msr6yxagzxpcht7thwa5bus5tomgz9';
-		$rootScope.rb_version = '7.4.2';
+		$rootScope.rb_version = '7.4.3';
 		$rootScope.total_faucet = 340282366;
 	})
 
