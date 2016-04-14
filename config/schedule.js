e26aa0060341704b405fb617e0a3591b398053af
wlemahieu <wesleylemahieu@gmail.com>
2016-04-14 16:23:34 +0000

    Automating 2hr distributions

--- config/schedule.js
+++ config/schedule.js
@@ -3,20 +3,44 @@
  */
 module.exports.schedule = {
 
-
     sailsInContext: true,
     tasks: {
         firstTask : {
-        cron : "* * * * *",
-        task : function (context, sails) {
-            if(sails.config.port === 1337) {
-                
-            } 
-            else if(sails.config.port === 1338) {
 
-            } 
-        },
-        context : {}
+            // run every 2 hours
+            cron : "0 */2 * * *",
+            task : function (context, sails) {
+
+                // production-level CRON.
+                if(sails.config.port === 1338) {
+
+                    console.log('-------------------------------------------------------');
+                    console.log('-------------------------------------------------------');
+                    console.log('---------------- DISTRIBUTION RUNNING -----------------');
+                    console.log('-------------------------------------------------------');
+                    console.log('-------------------------------------------------------');
+
+                    // CALCULATE TOTALS
+                    AutomationService.processTotals(function(err, resp) {
+                        if(!err) {
+
+                            console.log(TimestampService.utc() + ' [ AutomationService.processTotals() ] (!err) ' + JSON.stringify(resp));
+                                
+                            // PROCESS DISTRIBUTION
+                            AutomationService.processPayouts(function(err, resp) {
+                                if(!err) {
+                                    console.log(TimestampService.utc() + ' [ AutomationService.processPayouts() ] (!err) ' + JSON.stringify(resp));
+                                } else {
+                                   console.log(TimestampService.utc() + ' [ AutomationService.processPayouts() ] (err) ' + JSON.stringify(err));
+                                }
+                            });
+                        } else {
+                            console.log(TimestampService.utc() + ' [ AutomationService.processTotals() ] (err) ' + JSON.stringify(err));
+                        }
+                    });
+                } 
+            },
+            context : {}
         }
     }
 };
