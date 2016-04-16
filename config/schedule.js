c1e54a967046aa265d093bfb62f4fb18015185c9
wlemahieu <wesleylemahieu@gmail.com>
2016-04-16 22:08:59 +0000

    Payouts minutely, calculations bi-hourly

--- config/schedule.js
+++ config/schedule.js
@@ -6,47 +6,56 @@ module.exports.schedule = {
     sailsInContext: true,
     tasks: {
 
-        // Calculate distribution totals & distribute, then save new available supply
-        distribution : {
+        // Calculate distribution totals every 2 hours
+        processTotals : {
 
-            // run every 2 hours
             cron : "0 */2 * * *",
             task : function (context, sails) {
 
                 // production-level CRON.
                 if(sails.config.port === 1338) {
 
-                    console.log('-------------------------------------------------------');
-                    console.log('-------------------------------------------------------');
-                    console.log('---------------- DISTRIBUTION RUNNING -----------------');
-                    console.log('-------------------------------------------------------');
-                    console.log('-------------------------------------------------------');
+                    console.log('---------------- PROCESSING TOTALS -----------------');
 
                     // CALCULATE TOTALS
                     AutomationService.processTotals(function(err, resp) {
                         if(!err) {
-
                             console.log(TimestampService.utc() + ' [ AutomationService.processTotals() ] (!err) ' + JSON.stringify(resp));
-                                
-                            // PROCESS DISTRIBUTION
-                            AutomationService.processPayouts(function(err, resp) {
-                                if(!err) {
-                                    console.log(TimestampService.utc() + ' [ AutomationService.processPayouts() ] (!err) ' + JSON.stringify(resp));
-                                
-                                   // LOAD AVAILABLE SUPPLY
-                                   AutomationService.loadAvailableSupply(function(err, resp) {
-                                        if(!err) {
-                                            console.log(TimestampService.utc() + ' [ AutomationService.loadAvailableSupply() ] (!err) ' + JSON.stringify(resp));
-                                        } else {
-                                            console.log(TimestampService.utc() + ' [ AutomationService.loadAvailableSupply() ] (err) ' + JSON.stringify(err));
-                                        }
-                                   });                                    
-                                } else {
-                                    console.log(TimestampService.utc() + ' [ AutomationService.processPayouts() ] (err) ' + JSON.stringify(err));
-                                }
-                            });
                         } else {
                             console.log(TimestampService.utc() + ' [ AutomationService.processTotals() ] (err) ' + JSON.stringify(err));
+                        }
+                    });
+                } 
+            },
+            context : {}
+        },
+
+        // Process payouts every minute (if they exist) and load available supply (if it changes)
+        processPayouts : {
+
+            cron : "* * * * *",
+            task : function (context, sails) {
+
+                // production-level CRON.
+                if(sails.config.port === 1338) {
+
+                    console.log('---------------- PROCESSING PAYOUTS -----------------');
+
+                    // PROCESS DISTRIBUTION
+                    AutomationService.processPayouts(function(err, resp) {
+                        if(!err) {
+                            console.log(TimestampService.utc() + ' [ AutomationService.processPayouts() ] (!err) ' + JSON.stringify(resp));
+                        
+                           // LOAD AVAILABLE SUPPLY
+                           AutomationService.loadAvailableSupply(function(err, resp) {
+                                if(!err) {
+                                    console.log(TimestampService.utc() + ' [ AutomationService.loadAvailableSupply() ] (!err) ' + JSON.stringify(resp));
+                                } else {
+                                    console.log(TimestampService.utc() + ' [ AutomationService.loadAvailableSupply() ] (err) ' + JSON.stringify(err));
+                                }
+                           });                                    
+                        } else {
+                            console.log(TimestampService.utc() + ' [ AutomationService.processPayouts() ] (err) ' + JSON.stringify(err));
                         }
                     });
                 } 
@@ -64,11 +73,7 @@ module.exports.schedule = {
                 // development-level CRON.
                 if(sails.config.port === 1338) {
 
-                    console.log('-------------------------------------------------------');
-                    console.log('-------------------------------------------------------');
                     console.log('---------------- FIXING STUCK PENDING -----------------');
-                    console.log('-------------------------------------------------------');
-                    console.log('-------------------------------------------------------');
 
                     // FIND RECORDS OLDER THAN 1 MINUTE
                     AutomationService.fixStuckPending(function(err, resp) {
