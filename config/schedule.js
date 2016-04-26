98292def2cf52ba04db20cf6ba055c45fef7d9f6
wlemahieu <wesleylemahieu@gmail.com>
2016-04-26 19:57:17 +0000

    Payouts every 4 hours starting now

--- config/schedule.js
+++ config/schedule.js
@@ -9,7 +9,7 @@ module.exports.schedule = {
         // this finalizes calculations for the last period, then pays out participating accounts (every 2 hours)
         processDistribution : {
 
-            cron : "0 */2 * * *",
+            cron : "0 */4 * * *",
             task : function (context, sails) {
 
                 // production-level CRON.
